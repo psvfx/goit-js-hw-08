db6057beeeb71852f019dcda537adf9b147569f7
psvfx <psvfxinvest@gmail.com>
2023-06-18 00:11:24 +0300

    solution of task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// Initialize the Vimeo player
+const iframeEl = document.getElementById('vimeo-player');
+const player = new Player(iframeEl);
+
+// Event handler for time updates
+const handleTimeUpdate = throttle(time => {
+  // Save the current time to localStorage
+  localStorage.setItem('videoplayer-current-time', time);
+}, 1000);
+
+// Load the saved time and play the video from that time
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime).then(() => {
+    player.play();
+  });
+}
+
+// Track time updates and save to localStorage
+player.on('timeupdate', data => {
+  const currentTime = data.seconds;
+  handleTimeUpdate(currentTime);
+  // console.log(currentTime);
+});
